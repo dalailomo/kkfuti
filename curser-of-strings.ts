5dd07b6c6e9370879474f92e81c734e70d19b875
Peter <reisereise.85@gmail.com>
2022-11-11 16:54:20 +0100

    Create curser-of-strings.ts

---
+++ curser-of-strings.ts
@@ -0,0 +1,48 @@
+/**
+ * Ḡ̵̢̨̰̪̠̲̝̟̇ͨ̑ͭ͆͂̆͂͢r̴̨̙͍͖͉̀ͮ́͛ͨͨ̌ͥ͑̑̾͟ͅa̵̛̝̳̞̭̣͐̀͆̓̔̌ͧ̐́̌͘̚c̡̛͖̤̦͈̩̰̝̲̣͙̭̻̓̀̂̈́̚i̢̢̧̛̩͔ͮͭ̄̅̃ͮ͐͐̿̇̇͜͟a̴̡̧̘̥̣̱̘̝̓̅ͫ̐͋͒́̚̕͞ŝ̛͓̩̠̝̤̫̞̄̉̀ͤͫ̾̎̐͒̕ ̖̠ͩ͛̇͋͏̹͍̥͚̋̌ͥ̏ͥ̔̓̍p̧̛̛̭̲̯̞͖̹̎͆͛͆͐ͤ͆́̃̚ǒ̡̭̲̘͉͚̣̑͊̉͒ͩ̈́̒͊ͪ̚͢r̢̢̛͚̞͖̼͍̲̰͙ͬ̿̐͐́̓̿͢ ̸̻̞͚̼̮͉̗̖̤̦̈́̂ͣ̔ͦ̎̑̿u̥̰̤̦̤̘͓̩̖̼͑ͮ͂̓̌͑͋͟͞s̶̴̞̝̻͉̣̟̖̟̺͑͆̿̓̃̋ͪ͡ă̧̢̛̳̱̪̝͓̜̫̐̂͑ͥ̑ͤ͗ͅr̆͏̴̻̣͍̯͚̟̼ͧ̌̓̍̎̉͗̐͜ ̡̛̛̤̣͈̘͛ͪ̂̅̒ͨͫ̏̒̉̄͞ę̶̵̨̢̮̝͍̦̰͙͌̊̾ͣ̓̕͢͝s̵̸̡̠̝̦̩̭ͮ̒ͤ͒͗̊͗́̀̃͜ť̶̷̵̞͇̻̰ͥ̽͂̈ͭ͌̀̓̄ͤͪa̙̳̘̭̼̍̌̎̅̒̈́͂ͦ̋̑̅̚̚͡ ͉̣̝̱̱̒͊̓́̀̔̅̓̃͗̾ͬ̕̚p̶͇̜̻͚̱̦̔̎͐ͤͨͭ̆ͬͧ̚͢͠u̡̮̩̯̫̠̯̬̱͙͓̒ͪ̾̽̈́̅̓̋t̡̖̫̒̔̐̊̒͋ͧ̾̀͒̀̈́͗̈̕̚ą̩̻͇̮̗̥̰͌̐ͫ͛̊̀̿̈ͦ̚ͅ ̸̨͉̙͓͈͖͓̰͖̌ͦͨͭͦ̾͡͝͝m̷̡̖͔̬͕͕̱̓̓ͥͨ͌̒͌̾̕̕͞ḭ̷̗̦̲̯̠̺̳̿̔̅ͧ̾̏̓̌͋͝e̥̣̗ͨ̆̓ͬͫ̂ͤͫ͐ͮ̊̾̌͠͡͞r̶̶̢͈̩̼̳ͣ̉̈́̽̆̎́̃͘̕͡͝d̴͉͖̣͓̭̦̐̀̂̊̆̔̉ͧ͘̚͘ͅḁ̴̵̡̞̤̪̩̅̈ͮ̓̀̿̿͝͡͠͞ ̞̖͇̭͈̠̣̐͛ͧͣ̓̊̂ͤ̓̄ͮ̂i̸̧̢̻̰̰̜͇͇̘͑̉̄́ͪ̐̀̚͘n̴̡̯͉̞̥̰̙̍ͮ̍̑̽͊̾̓̔́̚ų̵̭̪̻͕̼̲̥̺͔̬̿ͣ̋̅̽̑͘t̸͎͖̼ͦ̀͠͞͏̡̨̩͇̳ͥ̄́̃̕i̸̵̷̢̭̯̯͉̼̺ͪ̆̓ͪ̇́̋͆͞l̷͕͙͖̲̱͔͔͚̟̫̇͂͋͒ͧ͐̆́,̢̻̥̜̻̞͓̗̜̂͂̍ͪ̃ͦ̇̕͡ͅ ̴̨͔̦͔̼̱̣̺̀̔̿ͫ͑͂̌̒̚͞b͔̬͔̏̎̑͐ͥ̑͗̿͢ͅ͏̶̰̉ͩ͗e̸̢̫̖͍̣͇̜̭̦ͬ̀̋͒̀̆͆̐͢s̷̛̱͉͙͈̖̫̠̓ͭͮ͐ͥ̇͊ͧͬͅo̸̷̡̡̢̝̼͇̠̘͉̠͗́̅ͬ͢͠ͅs̡̖͚͙̟͉̭͓̜̣̬̈̀͊̃̉͐͡͝
+ */
+
+// Usage: deno run curser-of-strings.ts <string_to_be_cursed:string> <number_of_iterations:number>
+
+import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+import { assert } from 'https://deno.land/std@0.157.0/testing/asserts.ts';
+
+const curseString = (s: string) => {
+    const getPainList = () => {
+        const a: string[] = [];
+
+        for (let i = 768; i < 879; i++) {
+            a.push('&#' + i + ';');
+        }
+
+        return a;
+    };
+
+    const doCursing = (input: string, painList: string[]): string => {
+        return input.split('').map(c => {
+            const randomPainListChar = painList[Math.ceil(Math.random() * painList.length) - 1];
+            return `${c}${randomPainListChar}`;
+        }).join('');
+    };
+
+    const cursedString = doCursing(s, getPainList());
+    const parsedCursed =  new DOMParser().parseFromString(`<h3>${cursedString}</h3>`, 'text/html');
+
+    assert(parsedCursed);
+    const h3 = parsedCursed.querySelector('h3');
+
+    assert(h3);
+    return h3.textContent;
+};
+
+const uncursedString = Deno.args[0];
+const iterations: number = parseInt(Deno.args[1] || '0');
+
+assert(uncursedString, "Missing string to curse as first argument");
+
+let result: string = curseString(uncursedString);
+for (let i = 0; i < iterations; i++) {
+    result = curseString(result);
+}
+
+console.debug(result);
